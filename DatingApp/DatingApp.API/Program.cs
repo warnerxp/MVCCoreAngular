ec37d47aeca79eba1c733d60a881f5ddc55ebf0f
Wagner Alvarado <warnerxp@outlook.com>
2019-03-25 08:34:37 -0600

    Test

--- DatingApp/DatingApp.API/Program.cs
+++ DatingApp/DatingApp.API/Program.cs
@@ -19,6 +19,6 @@ namespace DatingApp.API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>();  
     }
 }
