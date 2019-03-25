c5638ad4c0ab8a5365f529459fcf1ecb25f44af8
Wagner Alvarado <warnerxp@outlook.com>
2019-03-25 08:36:30 -0600

    Second Commit

--- DatingApp/DatingApp.API/Models/Value.cs
+++ DatingApp/DatingApp.API/Models/Value.cs
@@ -2,7 +2,8 @@ namespace DatingApp.API.Models
 {
     public class Value
     {
-         public int Id { get; set; }
+        // Test Commit
+        public int Id { get; set; }
         public string Name { get; set; }
         
     }
