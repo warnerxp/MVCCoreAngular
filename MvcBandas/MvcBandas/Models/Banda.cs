506638bc307846a349a89c244016b625278654f4
Wagner Alvarado <warnerxp@outlook.com>
2019-04-18 20:44:02 -0600

    Bandas Model

---
+++ MvcBandas/MvcBandas/Models/Banda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBandas.Models
+{
+    public class Banda
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Vocalista { get; set; }
+        public int NumeroIntegrantes { get; set; }
+        public int anioFormacion { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string Genero { get; set; }
+    }
+}
