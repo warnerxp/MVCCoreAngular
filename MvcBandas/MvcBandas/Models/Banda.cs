7225d9dea92c25d7a6344e352a841af6cffec196
Wagner Alvarado <warnerxp@outlook.com>
2019-04-20 21:54:41 -0600

    Vista Estilizadas

--- MvcBandas/MvcBandas/Models/Banda.cs
+++ MvcBandas/MvcBandas/Models/Banda.cs
@@ -9,13 +9,19 @@ namespace MvcBandas.Models
     public class Banda
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre de la banda es requerido.")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "El nombre del vocalista es requerido.")]
         public string Vocalista { get; set; }
         [Display(Name ="Número de Integrantes")]
+        [Required(ErrorMessage ="El número de integrantes es requerido.")]
         public int NumeroIntegrantes { get; set; }
         [Display(Name = "Año de formación")]
+        [Required(ErrorMessage = "El año de formación es requerido.")]
         public int anioFormacion { get; set; }
         [Display(Name = "Fecha de registro")]
+        [Required(ErrorMessage = "La fecha de registro es requerida.")]
+        [DataType(DataType.Date)]
         public DateTime FechaRegistro { get; set; }
         [Display(Name = "Género Musical")]
         public string Genero { get; set; }
