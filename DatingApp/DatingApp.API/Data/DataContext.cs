22b4ebd6af980f4b75cbad5d3df65d4a0fed8f57
warnerxp <warnerxp@outlook.com>
2019-11-06 13:40:38 -0600

    cmt
    cmt

--- DatingApp/DatingApp.API/Data/DataContext.cs
+++ DatingApp/DatingApp.API/Data/DataContext.cs
@@ -1,7 +1,13 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace DatingApp.API.Data
 {
-    public class DataContext
+    public class DataContext:DbContext  
     {
+        public DataContext(DbContextOptions<DataContext> options ): base (options){}
+
+        public DbSet<Value> Values {get;set;}
         
     }
 }
--- DatingApp/DatingApp.API/Startup.cs
+++ DatingApp/DatingApp.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using DatingApp.API.Models;
 
 namespace DatingApp.API
 {
@@ -25,6 +26,7 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Data.DataContext>(x => x.UseSqlite("ConnectionString"))
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
