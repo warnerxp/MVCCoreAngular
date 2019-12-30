284e9f44efaf4a53a121709d82b36271a6bd6b08
warnerxp <warnerxp@outlook.com>
2019-12-29 21:29:10 -0600

    Angular Guards completed

--- DatingApp/DatingApp-SPA/src/app/routes.ts
+++ DatingApp/DatingApp-SPA/src/app/routes.ts
@@ -6,10 +6,19 @@ import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-    { path: 'messages', component: MessagesComponent},
-    { path: 'lists', component: ListsComponent},
-    { path: '**', redirectTo: 'home', pathMatch: 'full'},
+    { path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            // { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+            { path: 'members', component: MemberListComponent},
+            { path: 'messages', component: MessagesComponent},
+            { path: 'lists', component: ListsComponent},
+        ]
+    },
+
+    { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
 
