f662af7948a13b21bb124d0415943ce5d00b299b
warnerxp <warnerxp@outlook.com>
2020-07-11 13:33:08 -0600

    Test GitLab

--- DatingApp/DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp/DatingApp.API/Controllers/PhotosController.cs
@@ -25,6 +25,7 @@ namespace DatingApp.API.Controllers
         private readonly IDatingRepository _repo;
         private readonly IMapper _mapper;
         private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
+
         private Cloudinary _cloudinary;
 
         public PhotosController(IDatingRepository repo,IMapper mapper,
